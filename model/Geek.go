43b793f2100b5312a352ec58a8dd9d0453c6341f
刘启明 <455043818@qq.com>
2023-02-28 19:25:18 +0800

    feat: update  gorm生成表名和字段名的规则

--- model/Geek.go
+++ model/Geek.go
@@ -4,8 +4,8 @@ import "gorm.io/gorm"
 
 type GeekCourseType struct {
 	gorm.Model          // 主键
-	TypeId       string `gorm:"type:varchar(255); not null" json:"type_id" binding:"required"`
-	TypeName     string `gorm:"type:varchar(255); not null" json:"type_name" binding:"required"`
-	DisplayOrder string `gorm:"type:int(11); not null" json:"displayOrder" binding:"required"`
-	IsDeleted    string `gorm:"type:int(1); not null" json:"isDeleted" binding:"required"`
+	TypeId       string `gorm:"column:typeId;type:varchar(255); not null" json:"typeId" binding:"required"`
+	TypeName     string `gorm:"column:typeName;type:varchar(255); not null" json:"typeName" binding:"required"`
+	DisplayOrder string `gorm:"column:displayOrder;type:int(11); not null" json:"displayOrder" binding:"required"`
+	IsDeleted    string `gorm:"column:isDeleted;type:int(1); not null" json:"isDeleted" binding:"required"`
 }
--- model/init.go
+++ model/init.go
@@ -9,6 +9,25 @@ import (
 	"gorm.io/gorm/schema"
 )
 
+// 定义一个自定义的命名策略
+type MyNamingStrategy struct {
+	schema.NamingStrategy
+}
+
+// 实现TableName方法，用于返回表名
+func (ns MyNamingStrategy) TableName(table string) string {
+	return table
+}
+
+// 实现ColumnName方法，用于返回列名
+func (ns MyNamingStrategy) ColumnName(table, column string) string {
+	// 如果列名包含CID，替换为Cid
+	// column = strings.ReplaceAll(column, "CID", "Cid")
+
+	// 返回列名
+	return column
+}
+
 var DB *gorm.DB
 
 func Database() {
@@ -16,9 +35,7 @@ func Database() {
 	// 需要更改的内容：用户名，密码，数据库名称
 	dsn := "root:M9y2512!@tcp(175.178.60.76:3306)/meta?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
-		NamingStrategy: schema.NamingStrategy{
-			SingularTable: true,
-		},
+		NamingStrategy: MyNamingStrategy{},
 	})
 
 	fmt.Println("db = ", db)
