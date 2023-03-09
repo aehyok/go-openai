6385b0b95bce15151f4edc873729f542ed5225ee
刘启明 <455043818@qq.com>
2023-03-09 20:09:48 +0800

    feat: 官网 token剩余的接口

--- routes/chatApi.go
+++ routes/chatApi.go
@@ -8,6 +8,7 @@ import (
 
 func ChatApi(v1 *gin.RouterGroup) {
 	v1.GET("/openai/getModels", Wrapper(gpt.GetModels))
+	v1.GET("/openai/getUsage", Wrapper(gpt.GetUsage))
 	v1.GET("/openai/getCompletions", Wrapper(gpt.GetCompletions))
 	v1.GET("/openai/getChatCompletions", gpt.GetChatCompletions)
 }
--- service/gpt/gpt.go
+++ service/gpt/gpt.go
@@ -1,11 +1,13 @@
 package gpt
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
 	"net/http"
 	"net/url"
+	"strings"
 
 	"geekdemo/model/dto"
 	"geekdemo/utils"
@@ -14,6 +16,34 @@ import (
 	openai "github.com/sashabaranov/go-openai"
 	"github.com/valyala/fasthttp"
 )
+
+func GetUsage(ctx *gin.Context) dto.ResponseResult {
+	url := utils.OpenAIUrl + `/dashboard/billing/credit_grants`
+
+	req := fasthttp.AcquireRequest()
+	defer fasthttp.ReleaseRequest(req)
+
+	req.SetRequestURI(url)
+	req.Header.SetMethod("GET")
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Bearer "+utils.OpenAIAuthToken)
+
+	resp := fasthttp.AcquireResponse()
+	defer fasthttp.ReleaseResponse(resp)
+
+	if err := fasthttp.Do(req, resp); err != nil {
+		fmt.Println("Error:", err)
+		return dto.SetResponseFailure("调用openai发生错误")
+	}
+
+	fmt.Println("Status:", resp.StatusCode())
+	var obj map[string]interface{}
+	if err := json.Unmarshal(resp.Body(), &obj); err != nil {
+		panic(err)
+	}
+	fmt.Println("Body:", obj)
+	return dto.SetResponseData(obj)
+}
 
 func GetModels(ctx *gin.Context) dto.ResponseResult {
 	url := utils.OpenAIUrl + `/v1/models`
@@ -73,6 +103,76 @@ func GetCompletions(ctx *gin.Context) dto.ResponseResult {
 	return dto.SetResponseData(obj["choices"])
 }
 
+type Event struct {
+	ID   string `json:"id"`
+	Data string `json:"data"`
+}
+
+func GetChatCompletionss(ctx *gin.Context) {
+	url := utils.OpenAIUrl + `/v1/chat/completions`
+
+	req := fasthttp.AcquireRequest()
+	defer fasthttp.ReleaseRequest(req)
+
+	req.SetRequestURI(url)
+	req.Header.SetMethod("POST")
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Bearer "+utils.OpenAIAuthToken)
+	//gpt-3.5-turbo-0301
+	req.SetBody([]byte(`{"stream": true,"model": "gpt-3.5-turbo","max_tokens": 500, "messages": [{"role": "user", "content": "go语言实现hello world 并解析一下"}] }`))
+
+	resp := fasthttp.AcquireResponse()
+	defer fasthttp.ReleaseResponse(resp)
+
+	if err := fasthttp.Do(req, resp); err != nil {
+		fmt.Println("Error:", err)
+		// return dto.SetResponseFailure("调用openai发生错误")
+		ctx.JSON(200, gin.H{"data": "调用openai发生错误"})
+	}
+	fmt.Println("Status:", resp.StatusCode())
+
+	bodyBytes := resp.Body()
+	events := make([]Event, 0)
+
+	// Split the response into individual events and decode them
+	for _, eventBytes := range bytes.Split(bodyBytes, []byte("\n\n")) {
+		eventString := strings.TrimSpace(string(eventBytes))
+		if eventString == "" {
+			continue
+		}
+		var event Event
+		err := json.Unmarshal([]byte(eventString), &event)
+		if err != nil {
+			fmt.Println("Error decoding event:", err)
+			continue
+		}
+		events = append(events, event)
+	}
+
+	// Do something with the decoded events here
+	fmt.Println(events)
+
+	var obj map[string]interface{}
+	if err := json.Unmarshal(resp.Body(), &obj); err != nil {
+		panic(err)
+	}
+	fmt.Println("Body:", obj)
+	// return dto.SetResponseData(obj["choices"])
+	ctx.JSON(200, gin.H{"data": obj})
+	// for i := 0; i < int(resp.Body())(); i++ {
+	// 	chunk := resp.BodyBuffer().Bytes()[i : i+1]
+	// 	if _, err := ctx.Write(chunk); err != nil {
+	// 		fmt.Println(err)
+	// 		return
+	// 	}
+	// }
+
+	// // 释放请求和响应对象内存
+	// fasthttp.ReleaseRequest(req)
+	// fasthttp.ReleaseResponse(resp)
+	// return dto.SetResponseData(obj["choices"])
+}
+
 func GetChatCompletions(ctx *gin.Context) {
 	url := utils.OpenAIUrl + `/v1/chat/completions`
 
@@ -86,7 +186,7 @@ func GetChatCompletions(ctx *gin.Context) {
 	// req.Header.Set("Content-Type", "application/octet-stream")
 	req.Header.Set("Authorization", "Bearer "+utils.OpenAIAuthToken)
 	//gpt-3.5-turbo-0301
-	req.SetBody([]byte(`{"model": "gpt-3.5-turbo","max_tokens": 4096, "messages": [{"role": "user", "content": "go语言实现hello world 并解析一下"}] }`))
+	req.SetBody([]byte(`{"model": "gpt-3.5-turbo", "messages": [{"role": "user", "content": "go语言实现hello world并解析 "}] }`))
 
 	resp := fasthttp.AcquireResponse()
 	defer fasthttp.ReleaseResponse(resp)
